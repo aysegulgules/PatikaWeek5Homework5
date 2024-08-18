4563ce71bfb16b4e1b61a98bc8d4eeb98d4b400d
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-19 01:43:36 +0300

    Add project files.

---
+++ PatikaWeek5Homework5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz..:");
+
+numberGo:
+try 
+{
+ double number = double.Parse(Console.ReadLine());
+    Console.WriteLine("Girdiğiniz sayının karesi..:"+ Math.Pow(number,2));//Girilen sayının karesi yazdırılıyor.
+}
+catch (FormatException ) //girilen değerin formatı uygun olmadığında çalışır.
+{ 
+    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+    goto numberGo;
+}
+
